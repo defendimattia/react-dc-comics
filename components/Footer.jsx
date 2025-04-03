e2842b55ddfd5645c403eddcd023b1dca249958d
defendimattia <defendi.mattia92@gmail.com>
2025-04-03 21:09:03 +0200

    add: footer-links-container style and imgs

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,37 +4,39 @@ function Footer() {
     return (<footer>
         <FooterBlueBar />
         <div className="footer-links-container">
-            <div className="link-card"><span>DC COMICS</span>
-                <ul>
-                    <a href="#"><li>Characters</li></a>
-                    <a href="#"><li>Comics</li></a>
-                    <a href="#"><li>Movies</li></a>
-                    <a href="#"><li>TV</li></a>
-                    <a href="#"><li>Games</li></a>
-                    <a href="#"><li>Videos</li></a>
-                    <a href="#"><li>News</li></a>
-                </ul>
-                <span>SHOP</span>
-                <ul>
-                    <a href="#"><li>Shop DC</li></a>
-                    <a href="#"><li>Shop DC Collectibles</li></a>
-                </ul>
-            </div>
-            <div className="link-card">
-                <span>DC</span>
-                <ul>
-                    <a href="#"><li>Terms Of Use</li></a>
-                    <a href="#"><li>Privacy policy (New)</li></a>
-                    <a href="#"><li>Ad Choices</li></a>
-                    <a href="#"><li>Advertising</li></a>
-                    <a href="#"><li>Jobs</li></a>
-                    <a href="#"><li>Subscriptions</li></a>
-                    <a href="#"><li>Talent Workshops</li></a>
-                    <a href="#"><li>CPSC Certificates</li></a>
-                    <a href="#"><li>Ratings</li></a>
-                    <a href="#"><li>Shop Help</li></a>
-                    <a href="#"><li>Contact Us</li></a>
-                </ul>
+            <div className="container">
+                <div className="link-card"><span>DC COMICS</span>
+                    <ul>
+                        <a href="#"><li>Characters</li></a>
+                        <a href="#"><li>Comics</li></a>
+                        <a href="#"><li>Movies</li></a>
+                        <a href="#"><li>TV</li></a>
+                        <a href="#"><li>Games</li></a>
+                        <a href="#"><li>Videos</li></a>
+                        <a href="#"><li>News</li></a>
+                    </ul>
+                    <span>SHOP</span>
+                    <ul>
+                        <a href="#"><li>Shop DC</li></a>
+                        <a href="#"><li>Shop DC Collectibles</li></a>
+                    </ul>
+                </div>
+                <div className="link-card">
+                    <span>DC</span>
+                    <ul>
+                        <a href="#"><li>Terms Of Use</li></a>
+                        <a href="#"><li>Privacy policy (New)</li></a>
+                        <a href="#"><li>Ad Choices</li></a>
+                        <a href="#"><li>Advertising</li></a>
+                        <a href="#"><li>Jobs</li></a>
+                        <a href="#"><li>Subscriptions</li></a>
+                        <a href="#"><li>Talent Workshops</li></a>
+                        <a href="#"><li>CPSC Certificates</li></a>
+                        <a href="#"><li>Ratings</li></a>
+                        <a href="#"><li>Shop Help</li></a>
+                        <a href="#"><li>Contact Us</li></a>
+                    </ul>
+                </div>
                 <div className="link-card">
                     <span>SITES</span>
                     <ul>
@@ -45,9 +47,11 @@ function Footer() {
                         <a href="#"><li>DC Power Visa</li></a>
                     </ul>
                 </div>
+
+                {/*logo*/}
+                <a href=""><img src="dc-logo-bg.png" alt="" /></a>
             </div>
-            {/*logo*/ }
-            <a href=""></a> 
+
         </div>
         <div className="footer-social-container">
             {/* button */}
@@ -62,7 +66,7 @@ function Footer() {
                 <a href="#"><img src="" alt="maps" /></a>
             </div>
         </div>
-    </footer>)
+    </footer >)
 }
 
 export default Footer
