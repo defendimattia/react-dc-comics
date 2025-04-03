db51e462570dfbe13401434fbc3165ef70b88715
defendimattia <defendi.mattia92@gmail.com>
2025-04-03 15:44:23 +0200

    add: footer links container

---
+++ components/FooterBlueBar.jsx
@@ -0,0 +1,28 @@
+function FooterBlueBar() {
+    return (
+        <div className="blue-bar-footer"><ul>
+            <a href="#"><li>
+                <img src="" alt="" />
+                DIGITAL COMICS
+            </li></a>
+            <a href="#"><li>
+                <img src="" alt="" />
+                DC MERCHANDISE
+            </li></a>
+            <a href="#"><li>
+                <img src="" alt="" />
+                SUBSCRIPTION
+            </li></a>
+            <a href="#"><li>
+                <img src="" alt="" />
+                COMIC SHOP LOCATOR
+            </li></a>
+            <a href="#"><li>
+                <img src="" alt="" />
+                DV POWER VISA
+            </li></a>
+        </ul></div>
+    )
+}
+
+export default FooterBlueBar
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,19 +1,19 @@
 
 function Header() {
     return (<header>
-            <a href="#"><img src="" alt="logo" /></a>
-            <ul>
-                <a href="#"><li>COMICS</li></a>
-                <a href="#"><li>CHARACTERS</li></a>
-                <a href="#"><li>MOVIES</li></a>
-                <a href="#"><li>TV</li></a>
-                <a href="#"><li>GAMES</li></a>
-                <a href="#"><li>COLLECTIBLES</li></a>
-                <a href="#"><li>VIDEOS</li></a>
-                <a href="#"><li>FANS</li></a>
-                <a href="#"><li>NEWS</li></a>
-                <a href="#"><li>SHOP</li></a>
-            </ul>
+        <a href="#"><img src="" alt="logo" /></a>
+        <ul>
+            <a href="#"><li>COMICS</li></a>
+            <a href="#"><li>CHARACTERS</li></a>
+            <a href="#"><li>MOVIES</li></a>
+            <a href="#"><li>TV</li></a>
+            <a href="#"><li>GAMES</li></a>
+            <a href="#"><li>COLLECTIBLES</li></a>
+            <a href="#"><li>VIDEOS</li></a>
+            <a href="#"><li>FANS</li></a>
+            <a href="#"><li>NEWS</li></a>
+            <a href="#"><li>SHOP</li></a>
+        </ul>
     </header>)
 }
 
