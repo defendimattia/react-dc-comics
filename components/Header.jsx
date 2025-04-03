77cc28c489a2dc74ba107b9ae774820a5c216669
defendimattia <defendi.mattia92@gmail.com>
2025-04-03 15:17:54 +0200

    add: header structure

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,20 @@
+
 function Header() {
-    return (<header>header</header>)
+    return (<header>
+            <a href="#"><img src="" alt="logo" /></a>
+            <ul>
+                <a href="#"><li>COMICS</li></a>
+                <a href="#"><li>CHARACTERS</li></a>
+                <a href="#"><li>MOVIES</li></a>
+                <a href="#"><li>TV</li></a>
+                <a href="#"><li>GAMES</li></a>
+                <a href="#"><li>COLLECTIBLES</li></a>
+                <a href="#"><li>VIDEOS</li></a>
+                <a href="#"><li>FANS</li></a>
+                <a href="#"><li>NEWS</li></a>
+                <a href="#"><li>SHOP</li></a>
+            </ul>
+    </header>)
 }
 
 export default Header
