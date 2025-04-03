f7dff21d2dbb73ad3050e8db2361433097ef8012
defendimattia <defendi.mattia92@gmail.com>
2025-04-03 16:35:27 +0200

    add: header style

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,21 +1,23 @@
 
 function Header() {
     return (<header>
-        {/* logo */}
-        <a href="#"><img src="" alt="logo" /></a>
-        {/* menu */}
-        <ul>
-            <a href="#"><li>COMICS</li></a>
-            <a href="#"><li>CHARACTERS</li></a>
-            <a href="#"><li>MOVIES</li></a>
-            <a href="#"><li>TV</li></a>
-            <a href="#"><li>GAMES</li></a>
-            <a href="#"><li>COLLECTIBLES</li></a>
-            <a href="#"><li>VIDEOS</li></a>
-            <a href="#"><li>FANS</li></a>
-            <a href="#"><li>NEWS</li></a>
-            <a href="#"><li>SHOP</li></a>
-        </ul>
+        <nav>
+            {/* logo */}
+            <a href=""><img src="/dc-logo.png" alt="logo" /></a>
+            {/* menu */}
+            <ul className="menu-list">
+                <a href="#"><li>CHARACTERS</li></a>
+                <a href="#"><li>COMICS</li></a>
+                <a href="#"><li>MOVIES</li></a>
+                <a href="#"><li>TV</li></a>
+                <a href="#"><li>GAMES</li></a>
+                <a href="#"><li>COLLECTIBLES</li></a>
+                <a href="#"><li>VIDEOS</li></a>
+                <a href="#"><li>FANS</li></a>
+                <a href="#"><li>NEWS</li></a>
+                <a href="#"><li>SHOP</li></a>
+            </ul>
+        </nav>
     </header>)
 }
 
