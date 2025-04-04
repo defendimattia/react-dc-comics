da03649c5c0ed6624d16766ef76caff48de5bf6f
defendimattia <defendi.mattia92@gmail.com>
2025-04-04 10:48:15 +0200

    add: FooterLinksSetion component

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,59 +1,11 @@
 import FooterBlueBar from "./FooterBlueBar"
+import FooterLinksSection from "./FooterLinksSection"
 import FooterSocialBar from "./FooterSocialBar"
 
 function Footer() {
     return (<footer>
         <FooterBlueBar />
-        {/* links list */}
-        <div className="footer-links-container">
-            <div className="container">
-                <div className="link-card"><span>DC COMICS</span>
-                    <ul>
-                        <a href="#"><li>Characters</li></a>
-                        <a href="#"><li>Comics</li></a>
-                        <a href="#"><li>Movies</li></a>
-                        <a href="#"><li>TV</li></a>
-                        <a href="#"><li>Games</li></a>
-                        <a href="#"><li>Videos</li></a>
-                        <a href="#"><li>News</li></a>
-                    </ul>
-                    <span>SHOP</span>
-                    <ul>
-                        <a href="#"><li>Shop DC</li></a>
-                        <a href="#"><li>Shop DC Collectibles</li></a>
-                    </ul>
-                </div>
-                <div className="link-card">
-                    <span>DC</span>
-                    <ul>
-                        <a href="#"><li>Terms Of Use</li></a>
-                        <a href="#"><li>Privacy policy (New)</li></a>
-                        <a href="#"><li>Ad Choices</li></a>
-                        <a href="#"><li>Advertising</li></a>
-                        <a href="#"><li>Jobs</li></a>
-                        <a href="#"><li>Subscriptions</li></a>
-                        <a href="#"><li>Talent Workshops</li></a>
-                        <a href="#"><li>CPSC Certificates</li></a>
-                        <a href="#"><li>Ratings</li></a>
-                        <a href="#"><li>Shop Help</li></a>
-                        <a href="#"><li>Contact Us</li></a>
-                    </ul>
-                </div>
-                <div className="link-card">
-                    <span>SITES</span>
-                    <ul>
-                        <a href="#"><li>DC</li></a>
-                        <a href="#"><li>MAD Magazine</li></a>
-                        <a href="#"><li>DC Kids</li></a>
-                        <a href="#"><li>DC Universe</li></a>
-                        <a href="#"><li>DC Power Visa</li></a>
-                    </ul>
-                </div>
-
-                {/*logo*/}
-                <a href=""><img src="dc-logo-bg.png" alt="" /></a>
-            </div>
-        </div>
+        <FooterLinksSection />
         <FooterSocialBar />
     </footer >)
 }
---
+++ components/FooterLinksSection.jsx
@@ -0,0 +1,56 @@
+function FooterLinksSection() {
+    return (
+        <div className="footer-links-container">
+            <div className="container">
+                {/* link lists */}
+                <div className="link-card"><span>DC COMICS</span>
+                    <ul>
+                        <a href="#"><li>Characters</li></a>
+                        <a href="#"><li>Comics</li></a>
+                        <a href="#"><li>Movies</li></a>
+                        <a href="#"><li>TV</li></a>
+                        <a href="#"><li>Games</li></a>
+                        <a href="#"><li>Videos</li></a>
+                        <a href="#"><li>News</li></a>
+                    </ul>
+                    <span>SHOP</span>
+                    <ul>
+                        <a href="#"><li>Shop DC</li></a>
+                        <a href="#"><li>Shop DC Collectibles</li></a>
+                    </ul>
+                </div>
+                <div className="link-card">
+                    <span>DC</span>
+                    <ul>
+                        <a href="#"><li>Terms Of Use</li></a>
+                        <a href="#"><li>Privacy policy (New)</li></a>
+                        <a href="#"><li>Ad Choices</li></a>
+                        <a href="#"><li>Advertising</li></a>
+                        <a href="#"><li>Jobs</li></a>
+                        <a href="#"><li>Subscriptions</li></a>
+                        <a href="#"><li>Talent Workshops</li></a>
+                        <a href="#"><li>CPSC Certificates</li></a>
+                        <a href="#"><li>Ratings</li></a>
+                        <a href="#"><li>Shop Help</li></a>
+                        <a href="#"><li>Contact Us</li></a>
+                    </ul>
+                </div>
+                <div className="link-card">
+                    <span>SITES</span>
+                    <ul>
+                        <a href="#"><li>DC</li></a>
+                        <a href="#"><li>MAD Magazine</li></a>
+                        <a href="#"><li>DC Kids</li></a>
+                        <a href="#"><li>DC Universe</li></a>
+                        <a href="#"><li>DC Power Visa</li></a>
+                    </ul>
+                </div>
+
+                {/* logo */}
+                <a href=""><img src="dc-logo-bg.png" alt="" /></a>
+            </div>
+        </div>
+    )
+}
+
+export default FooterLinksSection
