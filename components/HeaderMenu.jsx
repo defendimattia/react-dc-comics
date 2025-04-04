1f7ffedfca5565faac67c2aa9d0b3b28aec625f4
defendimattia <defendi.mattia92@gmail.com>
2025-04-04 14:35:34 +0200

    add: array with objects with name and link (arrItemsMenu)

--- components/HeaderMenu.jsx
+++ components/HeaderMenu.jsx
@@ -1,5 +1,47 @@
-function HeaderMenu() {
+const arrItemsMenu = [
+    {
+        name: "characters",
+        link: "#"
+    },
+    {
+        name: "comics",
+        link: "#"
+    },
+    {
+        name: "movies",
+        link: "#"
+    },
+    {
+        name: "tv",
+        link: "#"
+    },
+    {
+        name: "games",
+        link: "#"
+    },
+    {
+        name: "collectibles",
+        link: "#"
+    },
+    {
+        name: "videos",
+        link: "#"
+    },
+    {
+        name: "fans",
+        link: "#"
+    },
+    {
+        name: "news",
+        link: "#"
+    },
+    {
+        name: "shop",
+        link: "#"
+    },
+]
 
+function HeaderMenu() {
     return (<ul className="menu-list">
         <a href="#"><li>CHARACTERS</li></a>
         <a href="#" className="selected"><li>COMICS</li></a>
