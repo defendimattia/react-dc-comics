3ffd7b6542abb4b71fd077ee781c9b66c4ef350a
defendimattia <defendi.mattia92@gmail.com>
2025-04-07 15:23:43 +0200

    add: card component and data file

---
+++ src/components/Main/Card.jsx
@@ -0,0 +1,14 @@
+import style from "./Main.module.css"
+
+function Card() {
+
+    return (
+        <div className={style.card}>
+            <a href="_"><img src="_" alt="_" /></a>
+            <span></span>
+        </div>
+    )
+
+}
+
+export default Card
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,15 @@
 import Jumbotron from "./MainJumbotron"
 import style from "./Main.module.css"
+import Card from "./Card"
 
 function Main() {
     return (<main>
         <Jumbotron />
         <div className={style.container}>
             <strong className={style["current-series"]}>CURRENT SERIES</strong>
+            <div className={style["cards-container"]}>
+
+            </div>
             <button className={style["load-more"]}>LOAD MORE</button>
         </div>
 
